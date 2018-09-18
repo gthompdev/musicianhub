03f66deca41074f3ea098e365d030fed4cb76bf2
gthomp <cbk1011.gt@gmail.com>
2018-09-17 19:19:11 -0500

    Removed MusicianHub folder

--- MusicianHub/Dtos/AttendanceDto.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace MusicianHub.Dtos
-{
-    public class AttendanceDto
-    {
-        public int GigId { get; set; }
-    }
-}
--- MusicianHub/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MusicianHub.MvcApplication" Language="C#" %>
