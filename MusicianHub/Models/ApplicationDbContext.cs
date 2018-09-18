9a1cc3955c83bb984aadf95fe5f9c2896fb199f2
gthomp <cbk1011.gt@gmail.com>
2018-09-17 19:27:59 -0500

    Uploaded excluding .gitignore files

---
+++ MusicianHub/Models/ApplicationDbContext.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System.Data.Entity;
+
+namespace MusicianHub.Models
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public DbSet<Gig> Gigs { get; set; }
+        public DbSet<Genre> Genres { get; set; }
+        public DbSet<Attendance> Attendances { get; set; }
+
+        public ApplicationDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+
+        /*A user may attend many gigs, and a gig may have many users attending,
+         (many to many relationship). Once a user has attended a gig, or deletes their
+         account, we don't want to accidentally delete the gig as well.*/
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Attendance>()
+                .HasRequired(a => a.Gig)
+                .WithMany()
+                .WillCascadeOnDelete(false);
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ MusicianHub/Models/Genre.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MusicianHub.Models
+{
+    public class Genre
+    {
+        public byte Id { get; set; }
+
+        [Required] // resharper determines that "Required" is part of System.ComponentModel.DataAnnotations and adds the using statement
+        [StringLength(255)]
+        public string Name { get; set; }
+    }
+}
